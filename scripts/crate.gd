51500b3c8512edad9fd299465b1964651af59fdb
AedinMC <aedin.cowan@ontariotechu.net>
2025-10-07 20:13:42 -0400

    added stuff

--- scripts/crate.gd
+++ scripts/crate.gd
@@ -9,12 +9,13 @@ extends Area2D
 
 var entered = false ## Body entered by player
 
-@onready var crate_ui = $CrateInvUI ## Call on the CrateInvUI node to access the script.
+@onready var crate_ui = $CrateInvUI ## Call on the CrateInvUI node to access the script
+@onready var highlight = $Highlight ## Access to the highlight node
 
 @export var inv: CrateInv ## Crates use a CrateInv object to hold items
 @export var crate_id: String = "" ## Crate ID string to store information
 
-var player = null ## Set player to null so it can be updated in the future. 
+var player = null ## Set player to null so it can be updated in the future
 
 ## Add all crates to a group to ensure crate information is stored between scenes.
 ## Load the data and update the slot of the crate's inventory.
@@ -26,10 +27,12 @@ func _ready():
 		for i in range(min(inv.slots.size(), saved_data.size())): 
 			inv.slots[i].item = saved_data[i]
 		crate_ui.update_slots()
+	
+	highlight.visible = false
 
 ## Open the crate ui when the player has entered the body.
 func _physics_process(delta: float): 
-	if entered and Input.is_action_pressed("right_click"): 
+	if entered and (Input.is_action_pressed("right_click") or Input.is_action_just_pressed("left_click")): 
 		if !crate_ui.is_open: 
 			crate_ui.open() 
 
@@ -45,7 +48,9 @@ func reset_inventory():
 func _on_crate_body_entered(body: CharacterBody2D) -> void: 
 	entered = true 
 	player = body 
-	crate_ui.player = body 
+	crate_ui.player = body
+	highlight.visible = true
 	
 func _on_crate_body_exited(body: CharacterBody2D) -> void: 
 	entered = false
+	highlight.visible = false
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,12 @@
 extends Control
 ## Menu scene that allows the player to play the game, experience the tutorial, or exit the game.
 
+@onready var version = $Version ## Access to the version node
+
+## Controls what version of the game the user is playing
+func _ready() -> void:
+	version.add_text("Version 1.0.1")
+
 ## Start the game by sending the player to the beginning scene.
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/floors/beginning.tscn")
